bf7007c9b5fff30db4d8b2a4e7d626421ad33d89
mahmoodhamd <116299051+mahmoodhamd@users.noreply.github.com>
2024-07-14 18:22:37 +0500

    new changes

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,7 +8,8 @@ import { cn } from "@/utils/cn";
 
 
 import { BackgroundGradientAnimation } from "./GradientBg";
-//import GridGlobe from "./GridGlobe";
+import { GlobeDemo } from "./GridGlobe";
+//import GridGlobe from './GridGlobe'; // Ensure this path is correct
 //import animationData from "@/data/confetti.json";
 //import MagicButton from "../MagicButton";
 
@@ -97,7 +98,7 @@ export const BentoGridItem = ({
         {id === 6 && (
           // add background animation , remove the p tag
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+            <div className="absolute z-50  flex items-center justify-center text-white font-bold">
 
             </div>
           </BackgroundGradientAnimation>
@@ -106,9 +107,8 @@ export const BentoGridItem = ({
         <div
           className={cn(
             titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
-          )}
-        >
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-48  flex flex-col px-5 p-5 lg:p-10"
+          )}>
           {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
@@ -116,14 +116,75 @@ export const BentoGridItem = ({
           {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
           {/* remove mb-2 mt-2 */}
           <div
-            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10  `}
           >
             {title}
           </div>
+          
+          
+           {id===2 && <GlobeDemo/>}
+             
+             {id===3 && (
+              <div className="flex gap-1 lg:gap-5 w-fit
+              absolute -right-3 lg:-right-2 ">
+                <div className="flex flex-col space-y-3
+                lg:space-y-8 ">
+                {['JavaScript','Node.js','Nest.js','CDK'].map
+                ((item)=>(
+                 <span key={item} className="py-2 lg:py-4
+                 lg:px-3 px-4 text-xs lg:text-base
+                 opacity-50 lg:opacity-100 rounded-lg
+                 text-center bg-[#10132E]">
+                  {item}
+                 </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg
+                text-center bg-[#10132e]"/>
+            
+                </div>
+
+                <div className="flex flex-col gap-3
+                lg:gap-8">
+                {['Express.js','Python','Flask','AWS'].map
+                ((item)=>(
+                 <span key={item} className="py-2 lg:py-4
+                 lg:px-3 px-3 text-xs lg:text-base
+                 opacity-50 lg:opacity-100 rounded-lg
+                 text-center bg-[#10132E]">
+                  {item}
+                 </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg
+                text-center bg-[#10132e]"/>
+            
+                </div>
+              
+                <div className="flex flex-col gap-3
+                lg:gap-8 ">
+                  <span className="py-4 px-3 rounded-lg
+                text-center bg-[#10132e]"/>
+                {['laravel','Golang','Springboot','Lambda'].map
+                ((item)=>(
+                 <span key={item} className="py-2 lg:py-4
+                 lg:px-3 px-3 text-xs lg:text-base
+                 opacity-50 lg:opacity-100 rounded-lg
+                 text-center bg-[#10132E]">
+                  {item}
+                 </span>
+                ))}
+                
+            
+                </div>
 
 
-      </div>
+
+
+
+                </div>
+             )}
+     
     </div>
     </div>
+    </div>  
   );
 };
